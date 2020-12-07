49343bc23b0f4fa1e05d6636a7d26aa4183f9b8f
Marcin642 <baaltip@gmail.com>
2020-12-07 12:35:04 +0100

    projekt

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        animator.SetFloat("vertical", Input.GetAxis("Vertical"));
+        animator.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+
+
+        Jump();
+        Running();
+    }
+
+    public void Running()
+    {
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+
+    public void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("jump");
+        }
+    }
+}
